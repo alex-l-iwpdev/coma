42a04c7bb5366b3533451de379ad20ca3de3e6c2
Alex Lavigin <153559840+alex-l-iwpdev@users.noreply.github.com>
2025-06-21 12:43:14 +0200

    add GM

--- header.php
+++ header.php
@@ -67,8 +67,31 @@ $twitter   = ! empty( get_theme_mod( 'coma_twitter' ) ) ? get_theme_mod( 'coma_t
 				src="https://www.facebook.com/tr?id=1132185267839097&ev=PageView&noscript=1"/>
 	</noscript>
 	<!-- End Meta Pixel Code -->
+	<!-- Google Tag Manager -->
+	<script>( function( w, d, s, l, i ) {
+			w[ l ] = w[ l ] || [];
+			w[ l ].push( {
+				'gtm.start':
+					new Date().getTime(), event: 'gtm.js'
+			} );
+			var f = d.getElementsByTagName( s )[ 0 ],
+				j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore( j, f );
+		} )( window, document, 'script', 'dataLayer', 'GTM-M4F6GLBP' );</script>
+	<!-- End Google Tag Manager -->
+
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+	<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4F6GLBP"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <header>
 	<div class="top-header">
 		<div class="container">
